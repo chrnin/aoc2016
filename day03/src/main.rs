b5abd8e75f236aa2102d48fab8cfa4c03a821dab
Christophe <christophe@zbouboutchi.net>
2022-01-01 08:59:18 +0100

    day03 fix + day04

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,12 +1,9 @@
-use std::collections::HashSet;
 use std::fs;
 
 fn main() {
     println!("first: {}", first("input"));
     println!("second: {}", second("input"));
-
 }
-
 
 fn first(filename: &str) -> usize {
     let input = read_input(filename);
@@ -23,23 +20,22 @@ fn first(filename: &str) -> usize {
 fn second(filename: &str) -> usize {
     let input = read_input(filename);
     let mut count: usize = 0;
-    for i in 0..input.len()/3 {
-        let mut t0 = vec![input[i*3][0],input[i*3+1][0],input[i*3+2][0]];
-        let mut t1 = vec![input[i*3][1],input[i*3+1][1],input[i*3+2][1]];
-        let mut t2 = vec![input[i*3][2],input[i*3+1][2],input[i*3+2][2]];
+    for i in 0..input.len() / 3 {
+        let mut t0 = vec![input[i * 3][0], input[i * 3 + 1][0], input[i * 3 + 2][0]];
+        let mut t1 = vec![input[i * 3][1], input[i * 3 + 1][1], input[i * 3 + 2][1]];
+        let mut t2 = vec![input[i * 3][2], input[i * 3 + 1][2], input[i * 3 + 2][2]];
         t0.sort();
         t1.sort();
         t2.sort();
         if t0[0] + t0[1] > t0[2] {
             count += 1
         }
-        if t1[0] + t1[1] > t2[2] {
+        if t1[0] + t1[1] > t1[2] {
             count += 1
         }
         if t2[0] + t2[1] > t2[2] {
             count += 1
         }
-        println!("{:?} {:?} {:?}\n###", t0, t1, t2);
     }
     count
 }
@@ -51,8 +47,7 @@ fn read_input(filename: &str) -> Vec<Vec<u32>> {
             let l1 = line[2..5].trim().parse().unwrap();
             let l2 = line[7..10].trim().parse().unwrap();
             let l3 = line[12..15].trim().parse().unwrap();
-            let mut l = vec![l1, l2, l3];
-            return l;
+            return vec![l1, l2, l3];
         })
         .collect()
 }
--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,89 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!("first: {}", first("input"));
+    println!("second: {}", second("input"));
+}
+
+fn first(filename: &str) -> u32 {
+    let input = read_input(filename);
+    input
+        .iter()
+        .filter(|(name, _, checksum)| check(name, checksum))
+        .map(|(_, id, _)| id)
+        .sum()
+}
+
+fn second(filename: &str) -> u32 {
+    let input = read_input(filename);
+    let ciphers: Vec<_> = input
+        .iter()
+        .filter(|(name, _, checksum)| check(name, checksum))
+        .collect();
+    *ciphers
+        .iter()
+        .map(|(name, id, _)| {
+            (
+                name.chars().map(|c| translate(&c, id)).collect::<String>(),
+                id,
+            )
+        })
+        .filter(|(name, _)| name == "northpole object storage")
+        .nth(0)
+        .unwrap()
+        .1
+}
+fn check(name: &String, checksum: &String) -> bool {
+    let mut letters = HashMap::new();
+    for c in name.chars() {
+        if c != '-' {
+            *letters.entry(c).or_insert(0) += 1;
+        }
+    }
+    let mut check: Vec<&char> = letters.keys().collect();
+    check.sort_by(|&a, &b| order(a, b, &letters));
+    let true_checksum: &String = &check[..5].iter().map(|&c| c).collect();
+    true_checksum == checksum
+}
+
+fn translate(c: &char, id: &u32) -> char {
+    if c == &'-' {
+        return ' ';
+    } else {
+        ((((*c as u8 as u32 - 97) + id) % 26) + 97) as u8 as char
+    }
+}
+
+fn order(a: &char, b: &char, letters: &HashMap<char, i32>) -> Ordering {
+    if letters[a] > letters[b] {
+        return Ordering::Less;
+    } else if letters[a] == letters[b] {
+        if a < b {
+            return Ordering::Less;
+        } else {
+            return Ordering::Greater;
+        }
+    } else {
+        return Ordering::Greater;
+    }
+}
+
+fn read_input(filename: &str) -> Vec<(String, u32, String)> {
+    let content = fs::read_to_string(filename).expect("can't read");
+    let mut ret = Vec::new();
+    for line in content.lines() {
+        let pos = line.rfind('-').unwrap();
+        let sec = &line[pos + 1..];
+        let mut id_checksum = sec.split('[');
+        let id: u32 = id_checksum.next().unwrap().parse().unwrap();
+        let checksum = id_checksum.next().unwrap();
+        ret.push((
+            line[..pos].to_string(),
+            id,
+            checksum[..checksum.len() - 1].to_string(),
+        ));
+    }
+    ret
 }
